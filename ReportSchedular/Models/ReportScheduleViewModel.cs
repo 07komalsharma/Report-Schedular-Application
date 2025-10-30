aecbf42b17680e7e30687dc5a962da5acd296219
Komal Sharma <56592343+07komalsharma@users.noreply.github.com>
2025-10-30 22:46:26 +0530

    Add files via upload

---
+++ ReportSchedular/Models/ReportScheduleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportSchedular.Models
+{
+    public class ReportScheduleViewModel
+    {
+        public Schedular SchedularViewModel { get; set; }
+
+        public List<RepMaster> RepMasterViewModel { get; set; }
+
+    }
+   
+}
