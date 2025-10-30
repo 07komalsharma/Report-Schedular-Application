57013a2f7581e689190e4fc2d65e1e622540853c
Komal Sharma <56592343+07komalsharma@users.noreply.github.com>
2025-10-30 22:43:07 +0530

    Add files via upload

---
+++ Controller/XtraReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.IO;
+
+namespace ReportSchedular.Controllers
+{
+    internal class XtraReport
+    {
+        internal DataTable DataSource;
+        XtraReport report = new XtraReport();
+        public XtraReport()
+        {
+        }
+
+        public object stream { get; internal set; }
+
+        //internal void CreateDocument()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        internal void ExportToPdf(MemoryStream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        //internal void LoadLayout(string v)
+        //{
+        //    report.LoadLayout(v);
+        //}
+
+
+    }
+}
